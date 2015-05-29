011ba64a3c61553630e3769e74ac94f227d25e74
Erik Eppel <eceppel@gmail.com>
2015-05-29 11:51:58 -0700

    Adds notes to source code, plus README and notes.md

--- data_generator.js
+++ data_generator.js
@@ -15,10 +15,18 @@ window.users = Object.keys(streams.users);
 
 // utility function for adding tweets to our data structures
 var addTweet = function(newTweet){
-  var username = newTweet.user;
-  streams.users[username].push(newTweet);
-  streams.home.push(newTweet);
+  var username = newTweet.user; // accesses user stored in tweet.user and assigns it to variable username
+  streams.users[username].push(newTweet); // pushes new tweet to individual streams.users[index] array
+  streams.home.push(newTweet); // also pushes new tweet to streams.home array
 };
+/* 
+addTweet accepts a single argument, which is the tweet obj from generateRandomTweet:
+tweet = {
+  user: "randomly selected from streams.users array"
+  message: "randomly generated tweet"
+  created_at: new Date()
+}
+*/
 
 // utility function
 var randomElement = function(array){
@@ -39,22 +47,22 @@ var randomMessage = function(){
 
 // generate random tweets on a random schedule
 var generateRandomTweet = function(){
-  var tweet = {};
-  tweet.user = randomElement(users);
-  tweet.message = randomMessage();
-  tweet.created_at = new Date();
-  addTweet(tweet);
+  var tweet = {}; // local variable that holds an empty object literal
+  tweet.user = randomElement(users); // assigns user property to tweet obj using randomElement to pick user from streams.users
+  tweet.message = randomMessage(); // assigns message property to tweet obj using randomMessage
+  tweet.created_at = new Date(); // assings created_at property to tweet obj 
+  addTweet(tweet); // calls addTweet and passes it tweet obj
 };
 
 for(var i = 0; i < 10; i++){
   generateRandomTweet();
-}
+} // for loop calls generateRandomTweet() ten times to generate the initial batch of tweets shown on the page
 
 var scheduleNextTweet = function(){
   generateRandomTweet();
   setTimeout(scheduleNextTweet, Math.random() * 1500);
 };
-scheduleNextTweet();
+scheduleNextTweet(); 
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
